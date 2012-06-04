e9c29bf1a9c5d035e13d3147bf63cf37d0ec500f
l-lin <l-lin@users.noreply.github.com>
2012-06-04 08:36:31 +0000

    WORKFLOWNOTIFYDIRECTORY-21 : Update imports
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/directory-form-workflow/module-workflow-notifydirectory@41040 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/workflow/modules/notifydirectory/web/NotifyDirectoryTaskComponent.java
+++ src/java/fr/paris/lutece/plugins/workflow/modules/notifydirectory/web/NotifyDirectoryTaskComponent.java
@@ -43,8 +43,8 @@ import fr.paris.lutece.plugins.workflow.modules.notifydirectory.utils.constants.
 import fr.paris.lutece.plugins.workflow.service.WorkflowPlugin;
 import fr.paris.lutece.plugins.workflow.service.security.IWorkflowUserAttributesManager;
 import fr.paris.lutece.plugins.workflow.utils.WorkflowUtils;
-import fr.paris.lutece.plugins.workflow.web.task.NoFormTaskComponent;
 import fr.paris.lutece.plugins.workflowcore.service.task.ITask;
+import fr.paris.lutece.plugins.workflowcore.web.task.NoFormTaskComponent;
 import fr.paris.lutece.portal.service.i18n.I18nService;
 import fr.paris.lutece.portal.service.message.AdminMessage;
 import fr.paris.lutece.portal.service.message.AdminMessageService;
